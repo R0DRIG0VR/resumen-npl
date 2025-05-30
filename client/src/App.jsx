d03eebd261a8fffe9d3413deffc4b531c658c59f
RODRIGOVR <RodrigoVR70@gmail.com>
2025-05-30 00:50:42 -0400

    Add:Frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import  {Route, Routes} from 'react-router';
+function App(){
+    return(
+        <Routes>
+            <Route path="/" element={<h1>Home Page</h1>} />
+        </Routes>
+    )
+}
+
+export default App;
